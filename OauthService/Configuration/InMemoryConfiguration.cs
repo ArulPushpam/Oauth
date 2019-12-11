f8facd3422c07c8e711a340be0e7eaf5ebb7b78e
@ArulPushpam <arul.murugan@ameexusa.con>
2019-12-11 21:55:51 +0530

    initial push

---
+++ OauthService/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuthService.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[] {
+                new ApiResource("MicroServiceApp", "MicroService App")
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[] {
+                new Client
+                {
+                    ClientId = "MicroServiceApp",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "oauthservice" }
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[] {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "arul@test",
+                    Password = "password"
+                }
+            };
+        }
+    }
+}
---
+++ OauthService/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OAuthService.Configuration;
+
+namespace OAuthService
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                   .AddInMemoryClients(InMemoryConfiguration.Clients())
+                   .AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
+                   .AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Debug);
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+        }
+    }
+}
